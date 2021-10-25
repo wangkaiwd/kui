aac4abfbbc1774c3ec0175e05688abf02763cae2
wangkaiwd <1162486176@qq.com>
2021-10-25 23:57:48 +0800

    feat: add todo

--- src/components/tree/demo.vue
+++ src/components/tree/demo.vue
@@ -14,6 +14,7 @@ import { defineComponent, ref } from 'vue';
 import KTree from './tree.vue';
 import { treeData } from '@/components/tree/test-data';
 
+// todo: add vue3 new define emit props for tree component
 export default defineComponent({
   name: 'KTreeDemo',
   components: { KTree },
